2e18c624980c0295597866d2196621eeb021e361
bileamScheuvens <benedictbileam@gmx.de>
2026-04-26 17:39:58 +0200

    define env

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Baitless. Analog interface for youtube videos.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # Adjust if needed
+      pkgs = import nixpkgs { inherit system; };
+      python = pkgs.python3.override {
+        packageOverrides = new: old: {
+        };
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          (python.withPackages (
+            ps: with ps; [
+              yt-dlp
+              qrcode
+            ]
+          ))
+        ];
+
+        shellHook = ''
+          # Add necessary paths for dynamic linking
+          export LD_LIBRARY_PATH=${
+            pkgs.lib.makeLibraryPath (
+              [
+                "/run/opengl-driver" # Needed to find libGL.so
+              ]
+              ++ buildInputs
+            )
+          }:$LD_LIBRARY_PATH
+        '';
+      };
+    };
+}
